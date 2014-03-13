18dc0db91f417d28c61212312d81ca9f544b77f9
steph789 <stephanie.gerlich@gmail.com>
2014-03-13 16:12:05 +0100

    some changes to make it work without login

--- src/de/gerlich/fitnesscoachapp/CoachActivity.java
+++ src/de/gerlich/fitnesscoachapp/CoachActivity.java
@@ -2,14 +2,56 @@ package de.gerlich.fitnesscoachapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class CoachActivity extends Activity {
+	
+	Button login;
+	Button newaccount;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_coach);
+		
+		login = (Button) findViewById(R.id.buttonLogin);
+		newaccount = (Button) findViewById(R.id.buttonNewAccount);
+		
+		LoginListener ll = new LoginListener();
+		login.setOnClickListener(ll);
+		
+		NewAccountListener nal = new NewAccountListener();
+		newaccount.setOnClickListener(nal);
+		
+		
+	}
+	
+	
+	
+	
+	public class LoginListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	
+	public class NewAccountListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(CoachActivity.this, NewAccountActivity.class);
+			startActivity(intent);
+		}
+		
 	}
 
 	@Override
--- src/de/gerlich/fitnesscoachapp/NewAccountActivity.java
+++ src/de/gerlich/fitnesscoachapp/NewAccountActivity.java
@@ -1,7 +1,72 @@
 package de.gerlich.fitnesscoachapp;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class NewAccountActivity extends Activity{
+	
+	EditText editFirstname;
+	EditText editLastname;
+	EditText editUsername;
+	EditText editPassword;
+	EditText editEmail;
+	EditText editHeight;
+	RadioButton radioFemale;
+	RadioButton radioMale;
+	RadioGroup radioSex;
+	Spinner spinYearbirth;
+	
+	String firstname, lastname, username, password, email, heightt;
+	double height;
+	int yearbirth;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		editFirstname = (EditText) findViewById(R.id.editFirstname);
+		editLastname = (EditText) findViewById(R.id.editLastname);
+		editUsername = (EditText) findViewById(R.id.editUsername);
+		editPassword = (EditText) findViewById(R.id.editPassword);
+		editEmail = (EditText) findViewById(R.id.editEmail);
+		editHeight = (EditText) findViewById(R.id.editHeight);
+		radioSex = (RadioGroup) findViewById(R.id.radioGroupSex);
+		spinYearbirth = (Spinner) findViewById(R.id.spinnerYearbirth);
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.birthyears, android.R.layout.simple_spinner_dropdown_item);
+		spinYearbirth.setAdapter(adapter);
+
+		yearbirth = Integer.parseInt(spinYearbirth.getSelectedItem().toString());
+	}
+	
+	public class SaveListener implements OnClickListener{
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class SpinListener implements OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+			//Object item = parent.getItemAtPosition(pos);
+			
+		}
+		
+	}
 
 }
