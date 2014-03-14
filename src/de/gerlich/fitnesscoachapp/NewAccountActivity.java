ac268f1718e613fe9671717ff12b6c5012402d3c
steph789 <stephanie.gerlich@gmail.com>
2014-03-14 19:30:13 +0100

    creating new account basically works but

--- src/de/gerlich/fitnesscoachapp/NewAccountActivity.java
+++ src/de/gerlich/fitnesscoachapp/NewAccountActivity.java
@@ -1,19 +1,25 @@
 package de.gerlich.fitnesscoachapp;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import de.gerlich.fitnesscoachapp.database.TableUser;
+import de.gerlich.fitnesscoachapp.database.UserProvider;
 
-public class NewAccountActivity extends Activity{
-	
+public class NewAccountActivity extends Activity {
+
 	EditText editFirstname;
 	EditText editLastname;
 	EditText editUsername;
@@ -24,16 +30,27 @@ public class NewAccountActivity extends Activity{
 	RadioButton radioMale;
 	RadioGroup radioSex;
 	Spinner spinYearbirth;
-	
-	String firstname, lastname, username, password, email, heightt;
+	Button saveAccount;
+
+	String firstname, lastname, username, password, email;
 	double height;
-	int yearbirth;
-	
-	
+	int sex, yearbirth;
+
+	Uri saveUri;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_newaccount);
 		
+		Bundle extras = getIntent().getExtras();
+	    saveUri = (savedInstanceState == null) ? null : (Uri) savedInstanceState
+	        .getParcelable(UserProvider.CONTENT_ITEM_TYPE);
+	    if (extras != null) {
+	      saveUri = extras
+	          .getParcelable(UserProvider.CONTENT_ITEM_TYPE);
+	    }
+
 		editFirstname = (EditText) findViewById(R.id.editFirstname);
 		editLastname = (EditText) findViewById(R.id.editLastname);
 		editUsername = (EditText) findViewById(R.id.editUsername);
@@ -42,31 +59,61 @@ public class NewAccountActivity extends Activity{
 		editHeight = (EditText) findViewById(R.id.editHeight);
 		radioSex = (RadioGroup) findViewById(R.id.radioGroupSex);
 		spinYearbirth = (Spinner) findViewById(R.id.spinnerYearbirth);
+		saveAccount = (Button) findViewById(R.id.buttonSaveAccount);
+
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.birthyears,
+				android.R.layout.simple_spinner_item);
+		Log.d("Adapter", String.valueOf(adapter.getCount()));
 		
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.birthyears, android.R.layout.simple_spinner_dropdown_item);
 		spinYearbirth.setAdapter(adapter);
 
-		yearbirth = Integer.parseInt(spinYearbirth.getSelectedItem().toString());
-	}
-	
-	public class SaveListener implements OnClickListener{
+		saveAccount.setOnClickListener(new OnClickListener() {
 
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			
+			@Override
+			public void onClick(View v) {
+				firstname = editFirstname.getText().toString();
+				lastname = editLastname.getText().toString();
+				username = editUsername.getText().toString();
+				password = editPassword.getText().toString();
+				email = editEmail.getText().toString();
+				yearbirth = Integer.parseInt(spinYearbirth.getSelectedItem().toString());
+				height =Double.parseDouble(editHeight.getText().toString());
+				sex = radioSex.getCheckedRadioButtonId();
+				Log.d("onClick", String.valueOf(radioSex.getCheckedRadioButtonId()));
+			}
+		});
+	}
+
+
+	private void saveState() {
+
+		ContentValues values = new ContentValues();
+		values.put(TableUser.COLUMN_FIRSTNAME, firstname);
+		values.put(TableUser.COLUMN_LASTNAME, lastname);
+		values.put(TableUser.COLUMN_USERNAME, username);
+		values.put(TableUser.COLUMN_PASSWORD, password);
+		values.put(TableUser.COLUMN_EMAIL, email);
+		values.put(TableUser.COLUMN_HEIGHT, height);
+		values.put(TableUser.COLUMN_YEARBIRTH, yearbirth);
+		values.put(TableUser.COLUMN_SEX, sex);
+
+		if (saveUri == null) {
+			saveUri = getContentResolver().insert(UserProvider.CONTENT_URI,
+					values);
+
 		}
-		
-	}
-	
-	public class SpinListener implements OnItemClickListener {
 
-		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-			//Object item = parent.getItemAtPosition(pos);
-			
+		class SpinListener implements OnItemClickListener {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int pos,
+					long id) {
+				// Object item = parent.getItemAtPosition(pos);
+
+			}
+
 		}
-		
-	}
 
+	}
 }
